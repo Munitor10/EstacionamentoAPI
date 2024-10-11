9d6f71c11f0b8af2cbfa59b1fadad001289280cd
Munitor10 <rafamartinsmb10@gmail.com>
2024-10-10 21:51:59 -0300

    EstacionamentoAPI-Part1

---
+++ AlunoAPIv2/Program.cs
@@ -0,0 +1,60 @@
+using AlunoAPIv2.Repositores;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Habilitando o Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueClient",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:8080")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Realiza a leitura da conexão com o banco
+builder.Services.AddSingleton<PessoaRepository>(
+    provider =>
+    new PessoaRepository(builder.Configuration
+    .GetConnectionString("DefaultConnection")));
+
+//Swagger Parte 1
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
+
+var app = builder.Build();
+
+//Cors
+app.UseCors("AllowVueClient");
+
+//Swagger Parte 2
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crud Pessoa V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
